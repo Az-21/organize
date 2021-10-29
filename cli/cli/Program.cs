9c98b273e047f9e8ecff6fcbb49261173eccf494
Az-21 <39803478+Az-21@users.noreply.github.com>
2021-10-29 19:33:52 +0530

    project init + bolierplate

---
+++ cli/cli/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace Organize;
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine($"{"Text".Pastel("#55DA74")} {" Text ".Pastel("#000").PastelBg("#55DA74")}");
+    }
+}
